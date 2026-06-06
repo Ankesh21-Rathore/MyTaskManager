e6d95a72fd6887bf06c51a5a0d9baa057cafce2f
Ankesh21-Rathore <istsrathoresaab@gmail.com>
2026-06-06 22:37:44 +0530

    Initial Commit

---
+++ MyRoutine/MyRoutineApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyRoutineApp.swift
+//  MyRoutine
+//
+//  Created by mac on 06/06/26.
+//
+
+import SwiftUI
+
+@main
+struct MyRoutineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
